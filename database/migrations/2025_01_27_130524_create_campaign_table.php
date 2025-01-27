8f6f67e4802981c6a35be67f0cfa7c83246ddc4f
Dominik Fehér <feherdominik97@gmail.com>
2025-01-27 15:39:18 +0100

    Revert "fix: table column type fixed"
    This reverts commit eccada567e60fab81510b14371b125c876ff71cd.

--- database/migrations/2025_01_27_130524_create_campaign_table.php
+++ database/migrations/2025_01_27_130524_create_campaign_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('campaigns', function (Blueprint $table) {
+        Schema::create('campaign', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->enum('status', ['active', 'paused', 'completed'])->default('paused');
